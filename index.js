4fb3d94cf31fc80139cc3865f3900949b5b83a8d
Hossen <mdmitho199911@gmail.com>
2022-08-24 00:57:42 +0600

    middleware redux-logger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { createStore, combineReducers } = require("redux");
+const { createStore,applyMiddleware} = require("redux");
+const { default: logger } = require("redux-logger");
 
 // products constants
 const GET_PRODUCTS = "GET_PRODUCTS";
@@ -16,9 +17,7 @@ const getProducts =()=>{
   return {
     type:GET_PRODUCTS,
   }
-  
   }
-  
   
   const addProduct =(product)=>{
     return {
@@ -38,7 +37,7 @@ const productReducer =(state=initialProductState,action)=>{
     case ADD_PRODUCT:
       return {
         product: [...state.products, action.payload],
-        numberofProducts: state.numberofProducts + 1,
+        numgerofProducts: state.numgerofProducts + 1,
       };
 
     default:
@@ -49,7 +48,7 @@ const productReducer =(state=initialProductState,action)=>{
 
 
 // // store
-const store = createStore(productReducer);
+const store = createStore(productReducer,applyMiddleware(logger));
 store.subscribe(()=>{
   console.log(store.getState());
 })
@@ -58,88 +57,7 @@ store.dispatch(getProducts())
 store.dispatch(addProduct("pen"));
 
 
-// cart constants
-const GET_CART_ITEMS = "GET_CART_ITEMS";
-const ADD_CART_ITEMS = "ADD_CART_ITEMS";
 
 
 
 
-
-// Cart STATE
-const initialCartState ={
-  cart: ["salt"],
-  numgerofCart:1,
-}
-
-
-// cart action
-const getCart =()=>{
-return {
-  type:GET_CART_ITEMS,
-}
-
-}
-
-
-const addCart =(product)=>{
-return {
-  type:ADD_CART_ITEMS,
-  payload:product,
-}
-
-}
-
-
-
-
-// Cart REducer
-const cartReducer =(state=initialCartState,action)=>{
-  switch (action.type) {
-    case GET_CART_ITEMS:
-      
-      return{
-        ...state
-      };
-    case ADD_CART_ITEMS:
-      
-      return {
-        cart: [...state.cart, action.payload],
-        numgerofCart: state.numgerofCart + 1,
-      };
-  
-    default:
-     return state;
-  }
-}
-
-
-
-
-// store
-const store1 = createStore(cartReducer);
-store1.subscribe(()=>{
-  console.log(store1.getState());
-})
-
-store1.dispatch(getCart())
-store1.dispatch(addCart("pennnnnnnnnnnn"));
-
-
-// cartReducer 
-
-const rootReducer = combineReducers({
-  productR:productReducer,
-  cartR:cartReducer
-})
-
-
-// store
-const store2 = createStore(rootReducer);
-store2.subscribe(() => {
-  console.log(store2.getState());
-});
-
-
-store2.dispatch(getCart())
-store2.dispatch(addCart("pennnnnnnnnnnnjkhjkhjhkj"));
